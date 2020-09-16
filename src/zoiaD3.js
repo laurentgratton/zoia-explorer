53da39a76277e7f61a3487f5510bb1cc768b47cd
Larry <laurentgratton@gmail.com>
2020-09-16 15:55:00 -0500

    Refactor JS into files

---
+++ src/zoiaD3.js
@@ -0,0 +1,302 @@
+var zoia;
+var currPage = 0;
+function gridData() {
+  var data = [];
+  var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+  var ypos = 1;
+  var width = 30;
+  var height = 30;
+  var click = 0;
+
+  // iterate for rows
+  for (var row = 0; row < 5; row++) {
+    data.push( [] );
+
+    // iterate for cells/columns inside rows
+    for (var column = 0; column < 8; column++) {
+      data[row].push({
+        x: xpos,
+        y: ypos,
+        width: width,
+        height: height,
+        click: click,
+        id: column + row * 8,
+        info: {}
+      })
+      // increment the x position. I.e. move it over by 50 (width variable)
+      xpos += width + 12;
+    }
+    // reset the x position after a row is complete
+    xpos = 1;
+    // increment the y position for the next row. Move it down 50 (height variable)
+    ypos += height + 12;
+  }
+  if(zoia){
+    zoia.pages[currPage].modules.map(module => {
+      module.blocks = zoiaTemplate.modules[module.type].calcBlocks({
+        "id": module.id,
+        "blocks":zoiaTemplate.modules[module.type].blocks,
+        "options": module.options
+      })
+      data[Math.floor(module.position / 8)][module.position % 8].info = {
+        "id": module.id,
+        "color" : zoiaTemplate.colors[module.color].hexColor,
+        "module": zoiaTemplate.modules[module.type],
+        "block": module.blocks[0],
+        "options": module.options
+      }
+
+      if(module.blocks.length > 1) {
+        var startX = (module.position  + 1) % 8;
+        var currY = Math.floor(module.position / 8);
+        var startY = currY;
+        var totalBlocks = 0;
+        currX = startX;
+        while (totalBlocks < module.blocks.length){
+          data[currY][currX].info = {
+            "id": module.id,
+            "color" : zoiaTemplate.colors[module.color].hexColor,
+            "module": zoiaTemplate.modules[module.type],
+            "block": module.blocks[currX - startX + ((currY- startY) * 8)],
+            "options": module.options
+          }
+          totalBlocks++;
+          currX++;
+          if(currX == 8){
+            currX = 0;
+            currY++;
+          }
+          if(currY === 5) {
+            break;
+          }
+        }
+      }
+    })
+    var pageText = zoia.pages[currPage].name || "Page# " + (currPage + 1);
+    pageText += " (" + (currPage + 1) + " of " + zoia.pages.length + ")";
+    document.getElementById('page-name').textContent = pageText;
+  }
+  return data;
+}
+
+var pageData;
+function initd3() {
+  var dispatcher = d3.dispatch('nodeSelectGrid', 'nodeSelectGraph');
+  var grid = d3.select("#grid")
+    .append("svg")
+    .attr("width","340px")
+    .attr("height","220px");
+  pageData = gridData();
+  var row = grid.selectAll(".row")
+    .data(pageData)
+    .enter().append("g")
+    .attr("class", "row");
+
+  var column = row.selectAll(".square")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+    .attr("class", function(d) {var classList = "square "; classList += "grid"+d.info.id; return classList})
+    .attr("x", function(d) { return d.x; })
+    .attr("y", function(d) { return d.y; })
+    .attr("width", function(d) { return d.width; })
+    .attr("height", function(d) { return d.height; })
+    .style("fill", function(d) { return d.info.color ? d.info.color : "#fff"})
+    .style("stroke", "#222")
+    .on('click', function(d) {
+      showModule(d);
+      dispatcher.call('nodeSelectGrid', this, d);
+    });
+  d3.select(".left-panel").append("button")
+    .text("Next Page")
+    .on("click", function(){
+      if(zoia){
+        currPage = (currPage + 1) % zoia.pages.length;
+        row = grid.selectAll(".row");
+        column = row.selectAll(".square");
+        pageData = gridData();
+        column.remove();
+        row.remove();
+        row = grid.selectAll(".row")
+          .data(pageData)
+          .enter().append("g")
+          .attr("class", "row");
+
+        column = row.selectAll(".square")
+          .data(function(d) { return d; })
+          .enter().append("rect")
+          .attr("class", function(d) {var classList = "square "; classList += "grid"+d.info.id; return classList})
+          .attr("x", function(d) { return d.x; })
+          .attr("y", function(d) { return d.y; })
+          .attr("width", function(d) { return d.width; })
+          .attr("height", function(d) { return d.height; })
+          .style("fill", function(d) { return d.info.color ? d.info.color : "#fff"})
+          .style("stroke", "#222")
+          .on('click', function(d) {
+            showModule(d);
+            dispatcher.call('nodeSelectGrid', this, d);
+          });
+      }
+    })
+
+
+
+  networkWidth = 800;
+  networkHeight = 800;
+  var modules = zoia.pages.map(p => p.modules).flat();
+  var svg = d3.select("#network-graph")
+    .append('svg');
+  var net = svg
+    .attr('width',networkWidth)
+    .attr('height', networkHeight)
+    .append('g');
+
+  var linkNode = net
+    .selectAll("line")
+    .data(zoia.connections)
+    .enter()
+    .append("g");
+  var link = linkNode
+    .append("line")
+    .style("stroke", "#aaa")
+
+
+
+  var node = net
+    .selectAll("circle")
+    .data(modules)
+    .enter()
+    .append("g");
+
+  var circles = node
+    .append("circle")
+    .attr("r", "10")
+    .style("fill", function(d) {return zoiaTemplate.colors[d.color].hexColor;})
+    .attr("id", function(d) {return "graph" + d.id})
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended))
+    .on('click', function(d) {
+      showModule(d);
+      dispatcher.call('nodeSelectGraph', this, d);
+    });
+
+  var labels = node
+    .append("text")
+    .attr("class", "node-labels")
+    .text(function(d) { return d.typeName})
+    .attr('x', 12)
+    .attr('y', 3)
+
+  var simulation = d3.forceSimulation(modules)
+    .force("link", d3.forceLink()
+      .id(function(d) { return d.id})
+      .links(zoia.connections)
+    )
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("center", d3.forceCenter(networkWidth / 2, networkHeight / 2))
+    .on("tick", ticked);
+
+  var drag_handler = d3.drag()
+    .on("start", drag_start)
+    .on("drag", drag_drag)
+    .on("end", drag_end);
+
+  drag_handler(node);
+
+  var zoom = d3.zoom();
+  var zoom_handler = zoom
+    .on("zoom", zoom_actions);
+
+  zoom_handler(svg);
+
+  function ticked(){
+    link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    })
+  }
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+  function drag_start(d) {
+    d3.select(this)
+      .attr("cx", d.x = d3.event.x  )
+      .attr("cy", d.y = d3.event.y  );
+  }
+
+  //make sure you can't drag the circle outside the box
+  function drag_drag(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function drag_end(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+  function zoom_actions(){
+    net.attr("transform", d3.event.transform)
+  }
+
+
+  dispatcher.on('nodeSelectGrid', function(item){
+    document.querySelectorAll('.highlighted').forEach(node => node.classList.toggle('highlighted'));
+    document.querySelectorAll('#graph'+item.info.id).forEach(node => node.classList.toggle('highlighted'));
+
+  })
+  dispatcher.on('nodeSelectGraph', function(item){
+    console.log(item)
+    currPage = item.page;
+    row = grid.selectAll(".row");
+    column = row.selectAll(".square");
+    pageData = gridData();
+    column.remove();
+    row.remove();
+    row = grid.selectAll(".row")
+      .data(pageData)
+      .enter().append("g")
+      .attr("class", "row");
+
+    column = row.selectAll(".square")
+      .data(function(d) { return d; })
+      .enter().append("rect")
+      .attr("class", function(d) {var classList = "square "; classList += "grid"+d.info.id; return classList})
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; })
+      .attr("width", function(d) { return d.width; })
+      .attr("height", function(d) { return d.height; })
+      .style("fill", function(d) { return d.info.color ? d.info.color : "#fff"})
+      .style("stroke", "#222")
+      .on('click', function(d) {
+        showModule(d);
+        dispatcher.call('nodeSelectGrid', this, d);
+      });
+
+    document.querySelectorAll('.highlighted').forEach(node => node.classList.toggle('highlighted'));
+    document.querySelectorAll('.grid' + item.id).forEach(node => node.classList.toggle('highlighted'));
+
+    var pageText = zoia.pages[currPage].name || "Page# " + (currPage + 1);
+    pageText += " (" + (currPage + 1) + " of " + zoia.pages.length + ")";
+    document.getElementById('page-name').textContent = pageText;
+  })
+}
---
+++ src/zoiaFileParser.js
@@ -0,0 +1,388 @@
+
+var fileInput = document.getElementById("browseOpen");
+var zoia = {};
+
+fileInput.onchange = function () {
+  var fr = new FileReader();
+  fr.onloadend = function () {
+    d3.select("body").select("button").remove();
+    d3.select("#grid").selectAll("svg").remove();
+    d3.select('#network-graph').selectAll("svg").remove();
+    zoia = {};
+    var result = this.result;
+    var hex = "";
+    for (var i = 0; i < this.result.length; i++) {
+      var byteStr = result.charCodeAt(i).toString(16);
+      if (byteStr.length < 2) {
+        byteStr = "0" + byteStr;
+      }
+      hex += " " + byteStr;
+    }
+    hex = hex.trim();
+    document.getElementById('patch-name').textContent = getName(hex);
+    getSize(hex);
+    var numMods = getNumModules(hex);
+    var modules = getModules(hex, numMods);
+    var numConnections = getNumConnections(hex, modules.endPos);
+    var connections = getConnections(hex, modules.endPos, numConnections);
+    var numPages = getNumberPages(hex, connections.endPos);
+    var pageNames = getPageNames(hex, connections.endPos, numPages);
+    zoia.pages = [];
+    new Set(modules.modules.map(p => p.page)).forEach(pageID => zoia.pages.push({id: pageID, name: pageNames.names ? pageNames.names[pageID] : "", modules: []}));
+    modules.modules.map(m => zoia.pages.find(c => c.id === m.page).modules.push(m));
+    zoia.connections = [];
+    zoia.connections = connections.connections;
+    if(pageNames && pageNames.names) {
+      pageNames.names.map((n, i) => {if(zoia.pages[i]) { zoia.pages[i].name = n}});
+    }
+    initd3();
+  };
+  fr.readAsBinaryString(this.files[0]);
+};
+var moduleLabels = [
+  "SV Filter",
+  "Audio Input",
+  "Audio Out",
+  "Aliaser",
+  "Sequencer",
+  "LFO",
+  "ADSR",
+  "VCA",
+  "Audio Multiply",
+  "Bit Crusher",
+  "Sample and Hold",
+  "OD & Distortion",
+  "Env Follower",
+  "Delay line",
+  "Oscillator",
+  "Pushbutton",
+  "Keyboard",
+  "CV Invert",
+  "Steps",
+  "Slew Limiter",
+  "MIDI Notes in",
+  "MIDI CC in",
+  "Multiplier",
+  "Compressor",
+  "Multi-filter",
+  "Plate Reverb",
+  "Buffer delay",
+  "All-pass filter",
+  "Quantizer",
+  "Phaser",
+  "Looper",
+  "In Switch",
+  "Out Switch",
+  "Audio In Switch",
+  "Audio Out Switch",
+  "Midi pressure",
+  "Onset Detector",
+  "Rythm",
+  "Noise",
+  "Random",
+  "Gate",
+  "Tremolo",
+  "Tone Control",
+  "Delay w/Mod",
+  "Stompswitch",
+  "Value",
+  "CV Delay",
+  "CV Loop",
+  "CV Filter",
+  "Clock Divider",
+  "Comparator",
+  "CV Rectify",
+  "Trigger",
+  "Stereo Spread",
+  "Cport EXP/CV in",
+  "Cport CV out",
+  "UI Button",
+  "Audio Panner",
+  "Pitch Detector",
+  "Pitch Shifter",
+  "Midi Note out",
+  "Midi CC out",
+  "Midi PC out",
+  "Bit Modulator",
+  "Audio Balance",
+  "Inverter",
+  "Fuzz",
+  "Ghostverb",
+  "Cabinet Sim",
+  "Flanger",
+  "Chorus",
+  "Vibrato",
+  "Env Filter",
+  "Ring Modulator",
+  "Hall Reverb",
+  "Ping Pong Delay",
+  "Audio Mixer",
+  "CV Flip Flop",
+  "Diffuser",
+  "Reverb Lite",
+  "Room Reverb",
+  "Pixel",
+  "Midi Clock In",
+  "Granular"
+]
+var colors = [
+  "",
+  "Blue",
+  "Green",
+  "Red",
+  "Yellow",
+  "Aqua",
+  "Magenta",
+  "White",
+  "Orange",
+  "Lime",
+  "Surf",
+  "Sky",
+  "Purple",
+  "Pink",
+  "Peach",
+  "Mango"
+]
+var showModule = (module) => {
+  if(module.info){
+    document.getElementById('modules').textContent = JSON.stringify(module.info);
+    var moduleName = module.info.module.name;
+    var blockName = module.info.block.name;
+    console.log(moduleName + " - " + blockName);
+  } else {
+    document.getElementById('modules').textContent = JSON.stringify(module);
+  }
+}
+var getSize = (patch) => {
+  var data = patch
+    .substr(0, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  var size = parseInt(data, 16);
+  return size;
+}
+var getName = (patch) => {
+  // get patch name hex
+  var name = patch.substr(12, 47)
+    .split(' ')
+    .map((i) => parseInt(i, 16))
+    .map(n => n === 0 ? 32 : n)
+    .map(c => String.fromCharCode(c))
+    .join('')
+    .trim();
+
+  return name;
+}
+var getNumConnections = (patch, endModules) => {
+  var data = patch.substr(endModules, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('')
+  return parseInt(data || 0, 16);
+}
+var getNumberPages = (patch, endConnections) => {
+  var numberPages = patch.substr(endConnections, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(numberPages || 0, 16);
+}
+var getPageNames = (patch, endConnections, numPages) => {
+  var pages = []
+  if(numPages === 0){
+    return pages;
+  }
+  var data = patch.substr(endConnections + 12, numPages * 16 * 3);
+  var currEnd = endConnections + 12;
+  var current = 0;
+  var names = [];
+  while(current < numPages){
+    var tempName = data
+      .substr(current * 16 * 3, 16 * 3 - 1)
+      .split(' ')
+      .filter(c => c!== "00")
+      .map(c => String.fromCharCode(parseInt(c, 16)))
+      .join('');
+    names.push(tempName)
+    current++;
+    currEnd += 16*3;
+  }
+  return {'names': names, 'endPos': currEnd};
+}
+var getConnections = (patch, endModules, numConnections) => {
+  var data = patch.substr(endModules + 12);
+  var current = 0;
+  var currentSize = 0;
+  var totalSize = endModules + 12;
+  var connections = [];
+  while(current < numConnections) {
+    var connection = data.substr(currentSize, 60);
+    var currConnection = {
+      'id': current,
+      'origin': {
+        'id': getConnectionSource(connection),
+        'output': getConnectionSourceOutput(connection)
+      },
+      'source': getConnectionSource(connection),
+      'destination': {
+        'id': getConnectionDestination(connection),
+        'input': getConnectionDestinationInput(connection)
+      },
+      'target':getConnectionDestination(connection),
+      'strength': getConnectionStrength(connection)
+    }
+    connections.push(currConnection);
+    currentSize += 60;
+    totalSize += 60;
+    current++;
+  }
+  return {'connections': connections, 'endPos': totalSize}
+}
+var getConnectionSource = (connection) => {
+  var source = connection.substr(0, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(source || 0, 16);
+}
+var getConnectionSourceOutput = (connection) => {
+  var source = connection.substr(12, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(source || 0, 16);
+}
+var getConnectionDestination = (connection) => {
+  var source = connection.substr(24, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(source || 0, 16);
+}
+var getConnectionDestinationInput = (connection) => {
+  var source = connection.substr(36, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(source || 0, 16);
+}
+var getConnectionStrength = (connection) => {
+  var source = connection.substr(48, 11)
+    .split(' ')
+    .filter(c => c != "00")
+    .join('');
+  return parseInt(source || 0, 16);
+}
+var getNumModules = (patch) => {
+  var data = patch.substr(60, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  var numModules = parseInt(data, 16);
+  document.getElementById('num-modules').textContent = numModules + ' modules';
+  return numModules;
+}
+var getModules = (patch, numModules) => {
+  var data = patch.substr(72);
+  var current = 0;
+  var totalSize = 0;
+  var container = document.getElementById('modules');
+  var modules = [];
+  while(current < numModules){
+    var currModuleSize = getModuleSize(data.substr(totalSize, 11)) * 4 * 3;
+    var moduleData = data.substr(totalSize, currModuleSize);
+    var moduleType = getModuleType(moduleData);
+    var position = getModuleGridPos(moduleData)
+    var numOptions = getModuleNumberOptions(moduleData)
+    var currModule = {
+      'id': current,
+      'type': moduleType,
+      'typeName': moduleLabels[moduleType],
+      'page': getModulePage(moduleData),
+      'position': position,
+      'coords': posToXY(position),
+      'color': getModuleColor(moduleData),
+      'userOptions': numOptions,
+      'options': getModuleOptions(moduleData, numOptions),
+      'minWidth': parseInt(zoiaTemplate.modules[moduleType].minBlocks)
+    }
+    modules.push(currModule);
+    totalSize += currModuleSize;
+    current++;
+  }
+  //debugger;
+  return {'endPos': totalSize + 72, 'modules': modules};
+}
+var getModuleSize = (sizeBytes) => {
+  var size = sizeBytes.substr(0, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(size, 16);
+}
+var getModuleType = (moduleData) => {
+  var type = moduleData.substr(12, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(type || 0, 16);
+}
+var getModulePage = (moduleData) => {
+  var page = moduleData.substr(36, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(page || 0, 16);
+}
+var getModuleColor = (moduleData) => {
+  var page = moduleData.substr(48, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(page || 0, 16);
+}
+var getModuleGridPos = (moduleData) => {
+  var page = moduleData.substr(60, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(page || 0, 16);
+}
+var posToXY = (pos) => {
+  return {y: Math.floor(pos / 5) + 1 , x: (pos % 8) + 1}
+}
+var getModuleNumberOptions = (moduleData) => {
+  var params = moduleData.substr(72, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(params || 0, 16) + 1;
+}
+var getModuleOptions = (moduleData, numOptions) => {
+  var options = [];
+  var currentEnd = 96;
+  for(var i = 0; i < numOptions; i++){
+    var tempOption = moduleData.substr(currentEnd, 3);
+    options.push(parseInt(tempOption, 16));
+    currentEnd += 3;
+  }
+  return options;
+}
+var getNumPages = (patch, optionStart) => {
+  var numPages = patch.substr(optionStart, 11)
+    .split(' ')
+    .filter(c => c !== "00")
+    .join('');
+  return parseInt(numPages, 16);
+}
+var getModuleLabel = (moduleData) => {
+  var label = moduleData.substr(data.length - 48, 47)
+    .split(' ')
+    .map(n => n === 0 ? 32 : n)
+    .map(c => String.fromCharCode(c))
+    .join('')
+    .trim();
+  debugger;
+  return label;
+}
