a0c492f80a1faf79fec983a93785c18e6644d172
Laurent Gratton <laurentgratton@gmail.com>
2021-05-15 07:56:49 -0500

    Refactored the patch parsing to it's own function that SHOULD be callable from an external browser (such as a Qt WebView), the webview only needs to send the binary string.

--- src/zoiaFileParser.js
+++ src/zoiaFileParser.js
@@ -2,48 +2,51 @@
 var fileInput = document.getElementById("browseOpen");
 var zoia = {};
 
+let qtLaunchPatch = function(result) {
+  d3.select("body").select("button").remove();
+  d3.select("#grid").selectAll("svg").remove();
+  d3.select('#network-graph').selectAll("svg").remove();
+  zoia = {};
+  currPage = 0;
+  document.getElementById('modules').textContent = "";
+  let hex = "";
+  for (let i = 0; i < result.length; i++) {
+    let byteStr = result.charCodeAt(i).toString(16);
+    if (byteStr.length < 2) {
+      byteStr = "0" + byteStr;
+    }
+    hex += " " + byteStr;
+  }
+  hex = hex.trim();
+  document.getElementById('patch-name').textContent = getName(hex);
+  getSize(hex);
+  let numMods = getNumModules(hex);
+  let modules = getModules(hex, numMods);
+  let numConnections = getNumConnections(hex, modules.endPos);
+  let connections = getConnections(hex, modules.endPos, numConnections);
+  let numPages = getNumberPages(hex, connections.endPos);
+  let pageNames = getPageNames(hex, connections.endPos, numPages);
+  zoia.pages = [];
+  new Set(modules.modules.map(p => p.page)).forEach(pageID => zoia.pages.push({id: pageID, name: pageNames.names ? pageNames.names[pageID] : "", modules: []}));
+  modules.modules.map(m => zoia.pages.find(c => c.id === m.page).modules.push(m));
+  zoia.connections = [];
+  zoia.connections = connections.connections;
+  if(pageNames && pageNames.names) {
+    pageNames.names.map((n, i) => {if(zoia.pages[i]) { zoia.pages[i].name = n}});
+  }
+  document.getElementById('grid').classList.add("show");
+  document.getElementById('loaded-message').classList.add("show");
+  initd3();
+}
+
 fileInput.onchange = function () {
-  var fr = new FileReader();
+  let fr = new FileReader();
   fr.onloadend = function () {
-    d3.select("body").select("button").remove();
-    d3.select("#grid").selectAll("svg").remove();
-    d3.select('#network-graph').selectAll("svg").remove();
-    zoia = {};
-    currPage = 0;
-    document.getElementById('modules').textContent = "";
-    var result = this.result;
-    var hex = "";
-    for (var i = 0; i < this.result.length; i++) {
-      var byteStr = result.charCodeAt(i).toString(16);
-      if (byteStr.length < 2) {
-        byteStr = "0" + byteStr;
-      }
-      hex += " " + byteStr;
-    }
-    hex = hex.trim();
-    document.getElementById('patch-name').textContent = getName(hex);
-    getSize(hex);
-    var numMods = getNumModules(hex);
-    var modules = getModules(hex, numMods);
-    var numConnections = getNumConnections(hex, modules.endPos);
-    var connections = getConnections(hex, modules.endPos, numConnections);
-    var numPages = getNumberPages(hex, connections.endPos);
-    var pageNames = getPageNames(hex, connections.endPos, numPages);
-    zoia.pages = [];
-    new Set(modules.modules.map(p => p.page)).forEach(pageID => zoia.pages.push({id: pageID, name: pageNames.names ? pageNames.names[pageID] : "", modules: []}));
-    modules.modules.map(m => zoia.pages.find(c => c.id === m.page).modules.push(m));
-    zoia.connections = [];
-    zoia.connections = connections.connections;
-    if(pageNames && pageNames.names) {
-      pageNames.names.map((n, i) => {if(zoia.pages[i]) { zoia.pages[i].name = n}});
-    }
-    document.getElementById('grid').classList.add("show");
-    document.getElementById('loaded-message').classList.add("show");
-    initd3();
+    qtLaunchPatch(this.result);
   };
   fr.readAsBinaryString(this.files[0]);
 };
-var moduleLabels = [
+let moduleLabels = [
   "SV Filter",
   "Audio Input",
   "Audio Out",
@@ -129,7 +132,7 @@ var moduleLabels = [
   "Midi Clock In",
   "Granular"
 ]
-var colors = [
+let colors = [
   "",
   "Blue",
   "Green",
@@ -147,29 +150,29 @@ var colors = [
   "Peach",
   "Mango"
 ]
-var showModule = (module) => {
+let showModule = (module) => {
   if(module.info){
     document.getElementById('modules').textContent = JSON.stringify(module.info);
-    var moduleName = module.info.module.name;
-    var blockName = module.info.block.name;
+    let moduleName = module.info.module.name;
+    let blockName = module.info.block.name;
     console.log(moduleName + " - " + blockName);
   } else {
     document.getElementById('modules').textContent = JSON.stringify(module);
   }
 }
-var getSize = (patch) => {
-  var data = patch
+let getSize = (patch) => {
+  let data = patch
     .substr(0, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
-  var size = parseInt(data, 16);
+  let size = parseInt(data, 16);
   return size;
 }
-var getName = (patch) => {
+let getName = (patch) => {
   // get patch name hex
-  var name = patch.substr(12, 47)
+  let name = patch.substr(12, 47)
     .split(' ')
     .map((i) => parseInt(i, 16))
     .map(n => n === 0 ? 32 : n)
@@ -179,33 +182,33 @@ var getName = (patch) => {
 
   return name;
 }
-var getNumConnections = (patch, endModules) => {
-  var data = patch.substr(endModules, 11)
+let getNumConnections = (patch, endModules) => {
+  let data = patch.substr(endModules, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('')
   return parseInt(data || 0, 16);
 }
-var getNumberPages = (patch, endConnections) => {
-  var numberPages = patch.substr(endConnections, 11)
+let getNumberPages = (patch, endConnections) => {
+  let numberPages = patch.substr(endConnections, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(numberPages || 0, 16);
 }
-var getPageNames = (patch, endConnections, numPages) => {
-  var pages = []
+let getPageNames = (patch, endConnections, numPages) => {
+  let pages = []
   if(numPages === 0){
     return pages;
   }
-  var data = patch.substr(endConnections + 12, numPages * 16 * 3);
-  var currEnd = endConnections + 12;
-  var current = 0;
-  var names = [];
+  let data = patch.substr(endConnections + 12, numPages * 16 * 3);
+  let currEnd = endConnections + 12;
+  let current = 0;
+  let names = [];
   while(current < numPages){
-    var tempName = data
+    let tempName = data
       .substr(current * 16 * 3, 16 * 3 - 1)
       .split(' ')
       .filter(c => c!== "00")
@@ -217,15 +220,15 @@ var getPageNames = (patch, endConnections, numPages) => {
   }
   return {'names': names, 'endPos': currEnd};
 }
-var getConnections = (patch, endModules, numConnections) => {
-  var data = patch.substr(endModules + 12);
-  var current = 0;
-  var currentSize = 0;
-  var totalSize = endModules + 12;
-  var connections = [];
+let getConnections = (patch, endModules, numConnections) => {
+  let data = patch.substr(endModules + 12);
+  let current = 0;
+  let currentSize = 0;
+  let totalSize = endModules + 12;
+  let connections = [];
   while(current < numConnections) {
-    var connection = data.substr(currentSize, 60);
-    var currConnection = {
+    let connection = data.substr(currentSize, 60);
+    let currConnection = {
       'id': current,
       'origin': {
         'id': getConnectionSource(connection),
@@ -246,68 +249,68 @@ var getConnections = (patch, endModules, numConnections) => {
   }
   return {'connections': connections, 'endPos': totalSize}
 }
-var getConnectionSource = (connection) => {
-  var source = connection.substr(0, 11)
+let getConnectionSource = (connection) => {
+  let source = connection.substr(0, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(source || 0, 16);
 }
-var getConnectionSourceOutput = (connection) => {
-  var source = connection.substr(12, 11)
+let getConnectionSourceOutput = (connection) => {
+  let source = connection.substr(12, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(source || 0, 16);
 }
-var getConnectionDestination = (connection) => {
-  var source = connection.substr(24, 11)
+let getConnectionDestination = (connection) => {
+  let source = connection.substr(24, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(source || 0, 16);
 }
-var getConnectionDestinationInput = (connection) => {
-  var source = connection.substr(36, 11)
+let getConnectionDestinationInput = (connection) => {
+  let source = connection.substr(36, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(source || 0, 16);
 }
-var getConnectionStrength = (connection) => {
-  var source = connection.substr(48, 11)
+let getConnectionStrength = (connection) => {
+  let source = connection.substr(48, 11)
     .split(' ')
     .reverse()
     .filter(c => c != "00")
     .join('');
   return parseInt(source || 0, 16);
 }
-var getNumModules = (patch) => {
-  var data = patch.substr(60, 11)
+let getNumModules = (patch) => {
+  let data = patch.substr(60, 11)
     .split(' ')
     .filter(c => c !== "00")
     .join('');
-  var numModules = parseInt(data, 16);
+  let numModules = parseInt(data, 16);
   document.getElementById('num-modules').textContent = numModules + ' modules';
   return numModules;
 }
-var getModules = (patch, numModules) => {
-  var data = patch.substr(72);
-  var current = 0;
-  var totalSize = 0;
-  var container = document.getElementById('modules');
-  var modules = [];
+let getModules = (patch, numModules) => {
+  let data = patch.substr(72);
+  let current = 0;
+  let totalSize = 0;
+  let container = document.getElementById('modules');
+  let modules = [];
   while(current < numModules){
-    var currModuleSize = getModuleSize(data.substr(totalSize, 11)) * 4 * 3;
-    var moduleData = data.substr(totalSize, currModuleSize);
-    var moduleType = getModuleType(moduleData);
-    var position = getModuleGridPos(moduleData)
-    var numOptions = getModuleNumberOptions(moduleData)
-    var currModule = {
+    let currModuleSize = getModuleSize(data.substr(totalSize, 11)) * 4 * 3;
+    let moduleData = data.substr(totalSize, currModuleSize);
+    let moduleType = getModuleType(moduleData);
+    let position = getModuleGridPos(moduleData)
+    let numOptions = getModuleNumberOptions(moduleData)
+    let currModule = {
       'id': current,
       'type': moduleType,
       'typeName': moduleLabels[moduleType],
@@ -326,77 +329,77 @@ var getModules = (patch, numModules) => {
   //debugger;
   return {'endPos': totalSize + 72, 'modules': modules};
 }
-var getModuleSize = (sizeBytes) => {
-  var size = sizeBytes.substr(0, 11)
+let getModuleSize = (sizeBytes) => {
+  let size = sizeBytes.substr(0, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(size, 16);
 }
-var getModuleType = (moduleData) => {
-  var type = moduleData.substr(12, 11)
+let getModuleType = (moduleData) => {
+  let type = moduleData.substr(12, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(type || 0, 16);
 }
-var getModulePage = (moduleData) => {
-  var page = moduleData.substr(36, 11)
+let getModulePage = (moduleData) => {
+  let page = moduleData.substr(36, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(page || 0, 16);
 }
-var getModuleColor = (moduleData) => {
-  var page = moduleData.substr(48, 11)
+let getModuleColor = (moduleData) => {
+  let page = moduleData.substr(48, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(page || 0, 16);
 }
-var getModuleGridPos = (moduleData) => {
-  var page = moduleData.substr(60, 11)
+let getModuleGridPos = (moduleData) => {
+  let page = moduleData.substr(60, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(page || 0, 16);
 }
-var posToXY = (pos) => {
+let posToXY = (pos) => {
   return {y: Math.floor(pos / 5) + 1 , x: (pos % 8) + 1}
 }
-var getModuleNumberOptions = (moduleData) => {
-  var params = moduleData.substr(72, 11)
+let getModuleNumberOptions = (moduleData) => {
+  let params = moduleData.substr(72, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(params || 0, 16) + 1;
 }
-var getModuleOptions = (moduleData, numOptions) => {
-  var options = [];
-  var currentEnd = 96;
-  for(var i = 0; i < numOptions; i++){
-    var tempOption = moduleData.substr(currentEnd, 3);
+let getModuleOptions = (moduleData, numOptions) => {
+  let options = [];
+  let currentEnd = 96;
+  for(let i = 0; i < numOptions; i++){
+    let tempOption = moduleData.substr(currentEnd, 3);
     options.push(parseInt(tempOption, 16));
     currentEnd += 3;
   }
   return options;
 }
-var getNumPages = (patch, optionStart) => {
-  var numPages = patch.substr(optionStart, 11)
+let getNumPages = (patch, optionStart) => {
+  let numPages = patch.substr(optionStart, 11)
     .split(' ')
     .reverse()
     .filter(c => c !== "00")
     .join('');
   return parseInt(numPages, 16);
 }
-var getModuleLabel = (moduleData) => {
-  var label = moduleData.substr(data.length - 48, 47)
+let getModuleLabel = (moduleData) => {
+  let label = moduleData.substr(data.length - 48, 47)
     .split(' ')
     .map(n => n === 0 ? 32 : n)
     .map(c => String.fromCharCode(c))
